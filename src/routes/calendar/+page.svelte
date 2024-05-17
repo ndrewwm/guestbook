f75f72be7c695f7c475d2a164a4e94debad7611a
ndrewwm <moore.andrew09@gmail.com>
2024-05-17 17:47:38 -0600

    Updates to the calendar

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,9 +1,78 @@
 <script>
+  import dayjs from "dayjs";
   import Calendar from "./Calendar.svelte";
+
+  // TODO: replace with data from the backend
+  let events = [
+    // Before current month
+    { name: "Emily", start: "2024-04-29", end: "2024-05-02" },
+
+    // Overlaps with me
+    { name: "Amanda", start: "2024-05-14", end: "2024-05-17" },
+    { name: "Andrew", start: "2024-05-17", end: "2024-05-18" },
+
+    // No overlap
+    { name: "Tyler", start: "2024-05-28", end: "2024-05-30"},
+
+    // Extends outside current month
+    { name: "Larry", start: "2024-05-31", end: "2024-06-03"},
+  ];
+
+  let today = dayjs();
+  $: month = today.format("MMMM");
+  $: year = today.format("YYYY");
+
+  function nextMonth() {
+    today = today.add(1, "month");
+  }
+
+  function prevMonth() {
+    today = today.subtract(1, "month");
+  }
+
+  function getCurrentDate() {
+    today = dayjs();
+  }
 </script>
 
-<div>
+<div class="parent">
   <div class="child">
-    <Calendar />
+    <div class="buttons">
+      <button on:click={prevMonth}>⬅️</button>
+      {month}, {year}
+      <button on:click={nextMonth}>➡️</button>
+      <button on:click={getCurrentDate}>Today</button>
+      
+      <label for="switch">
+        List
+        <input type="checkbox" name="list" id="list">
+      </label>
+    </div>
+    
+    {#key today}
+      <Calendar {month} {year} {events} />
+    {/key}
   </div>
 </div>
+
+<style>
+  .parent {
+    margin-top: 20px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  .child {
+    color: white;
+    opacity: 90%;
+    backdrop-filter: blur(10px);
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.3);
+    border-radius: 16px;
+  }
+
+  .buttons {
+    /* margin-top: 15px; */
+    padding-top: 10px;
+    padding-left: 15px;
+  }
+</style>
--- src/routes/calendar/Calendar.svelte
+++ src/routes/calendar/Calendar.svelte
@@ -1,3 +1,103 @@
-<script>
+<script lang="ts">
+  // Essentially worked from this REPL as a base: https://svelte.dev/repl/7626cce5508d4a73a6fe9d851e54d79a?version=4.2.0
+  import dayjs from "dayjs";
+  import type { Event } from "./calendar";
+  import { Day } from "./calendar";
+
+  export let month: string;
+  export let year: string;
+  export let events: Event[];
+  const weekdays = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+
+  function createDays() {
+    let firstDayOfMonth = dayjs(`${year}-${month}-01`);
+    let firstDayOfWeek = dayjs(firstDayOfMonth).format("dd");
+    let weekdayIndex = weekdays.indexOf(firstDayOfWeek);
+    let calendarStartDate = dayjs(firstDayOfMonth).subtract(weekdayIndex, 'day');
+
+    let days: Day[] = [];
+    for (let i = 0; i < 35; i++) {
+      let date = dayjs(calendarStartDate).add(i, "day").format("YYYY-MM-DD");
+      let day = new Day(date, events, month, year);
+      days = [...days, day];
+    }
+
+    return days;
+  }
+
+  let days = createDays();
 </script>
 
+<div class="calendar">
+  <div class="calendar-head">
+    <!-- <div class="monthName">{calendarMonth}, {calendarYear}</div> -->
+  </div>
+  <div class="calendar-body">
+    <div class="weekdays">
+      {#each weekdays as weekday}
+        <div class="weekday">{weekday}</div>
+      {/each}
+    </div>
+    <div class="days">
+      {#each days as day}
+        <div class="day">
+          <div class="dateContainer">
+            <div class="date">
+              {dayjs(day.date).format("D")}
+            </div>
+          </div>
+        </div>
+      {/each}
+    </div>
+  </div>
+</div>
+
+
+<style>
+  .calendar {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5em;
+    padding: 0 1em;
+  }
+
+  .calendar-head {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  /* .monthName {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.125em;
+  } */
+
+  .weekdays {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    justify-items: center;
+  }
+
+  .weekday {
+    padding: 1em 0;
+    border-radius: 16px;
+  }
+
+  .days {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    justify-items: stretch;
+  }
+
+  .day {
+    display: flex;
+    flex-direction: column;
+    height: 4.5em;
+    align-items: center;
+    gap: 0.5em;
+    cursor: pointer;
+  }
+</style>

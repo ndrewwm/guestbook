52d10a0048985dd3fcc8bdd7fb3dbe1ad1518587
ndrewwm <moore.andrew09@gmail.com>
2024-06-19 16:47:34 -0600

    Update /api/calendar to support approval/rejection of requests

--- src/lib/server/database/crudEvent.ts
+++ src/lib/server/database/crudEvent.ts
@@ -1,6 +1,6 @@
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 import { db } from "./db";
-import { InsertEvent, SelectEvent, eventsTable } from "./schema";
+import { InsertEvent, eventsTable } from "./schema";
 
 export async function createEvent(data: InsertEvent) {
   let event = await db
@@ -8,5 +8,37 @@ export async function createEvent(data: InsertEvent) {
     .values(data)
     .returning({ id: eventsTable.id });
 
-    return event.at(0);
+  return event.at(0);
+}
+
+export async function approveEvent(event_id: string | null | undefined) {
+  if (!event_id) {
+    return null;
+  }
+
+  let update = await db
+    .update(eventsTable)
+    .set({ approved: 1 })
+    .where(eq(eventsTable.id, event_id))
+    .returning({ id: eventsTable.id });
+
+  return update.at(0);
+}
+
+export async function rejectEvent(event_id: string) {
+  if (!event_id) {
+    return null;
+  }
+
+  let deleted = await db
+    .delete(eventsTable)
+    .where(
+      and(
+        eq(eventsTable.id, event_id),
+        eq(eventsTable.approved, 0),
+      )
+    )
+    .returning({ id: eventsTable.id });
+
+  return deleted.at(0);
 }
---
+++ src/routes/api/calendar/reject/+server.ts
@@ -0,0 +1,16 @@
+import { rejectEvent } from '$lib/server/database/crudEvent.js';
+import { json } from '@sveltejs/kit';
+
+export async function POST({ url }) {
+  const event_id = url.searchParams.get("id");
+  if (!event_id) {
+    return json({ message: "Provide an event_id." }, { status: 400 });
+  }
+
+  const result = await rejectEvent(event_id);
+  if (result === undefined || result === null) {
+    return json({ message: `event_id ${event_id} not found.` }, { status: 400 });
+  }
+
+  return json({ status: 200, message: "Request rejected." });
+}
